b282e373dccd383b8063fa1003253fdde843ad02
Melvin <Melvin-Ros@github.com>
2019-11-26 21:47:31 +0100

    ch changes

--- src/p1_turtlebot_pkg/src/generate_points.cpp
+++ src/p1_turtlebot_pkg/src/generate_points.cpp
@@ -3,25 +3,30 @@
 #include <time.h>
 #include <cstdio>
 #include <cstdlib>
-#include <vector>
 #include <visualization_msgs/Marker.h>
 using namespace std;
 //struct Coordinates { double X;double Y; };  
 /** function declarations **/
+struct coordinatepoint{
+	double x;
+	double y;
+};
 
-void generatePoints(vector<vector<double>> &points,double sizeInSquareMeters, double minXvalue, double minYvalue, double maxXvalue, double distancebetweenpoints); 
+void generatePoints(vector<coordinatepoint> &points,double sizeOnX, double sizeOnY, double minXvalue, double minYvalue, double distancebetweenpoints); 
 int main(int argc, char **argv)
 {
-	double sizeInSquareMeters, distancebetweenpoints,minXvalue, minYvalue, maxXvalue;
+	double sizeOnX, sizeOnY, distancebetweenpoints,minXvalue, minYvalue;
 
     ros::init(argc, argv, "example_node");
     ros::NodeHandle n("~");
 	ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
     ros::Rate loop_rate(50);
-	vector<vector<double>> Points;
+	vector<coordinatepoint> Points;
 	cout << "time to generate points input the following information: " << endl;
-	cout << "size of map in square Meters: ";
-	cin >> sizeInSquareMeters;
+	cout << "size of map on x axis : ";
+	cin >> sizeOnX;
+	cout << "size of map on y axis : ";
+	cin >> sizeOnY;
 	cout << endl;
 	cout << "minimum X value: ";
 	cin >> minXvalue;
@@ -29,21 +34,20 @@ int main(int argc, char **argv)
 	cout << "minimum y value: ";
 	cin >> minYvalue;
 	cout << endl;
-	cout << "maximum X value: ";
-	cin >> maxXvalue;
-	cout << endl;
 	cout << "distance between the points: ";
 	cin >> distancebetweenpoints;
 	cout << endl;
 
-	generatePoints(Points,sizeInSquareMeters,minXvalue,minYvalue,maxXvalue,distancebetweenpoints);
+	generatePoints(Points, sizeOnX,sizeOnY, minXvalue, minYvalue, distancebetweenpoints);
 
 	int count = 0;
 	//Draw points in Rviz
     while (ros::ok()&& count <= Points.size())
     {
-		double choiceX = Points[count][0];
-		double choiceY = Points[count][1];
+		double choiceX = Points[count].x;
+		double choiceY = Points[count].y;
+
+		cout << choiceX << " " << choiceY << endl;
 
 		//cout << count << " " << Points[count][0] << "  " << Points[count][1] <<endl;
 		//create a marker point
@@ -60,15 +64,16 @@ int main(int argc, char **argv)
       points.scale.y = 0.3;
 	  // Points are green
        points.color.g = 1.0f;
-       points.color.a = 1.0;
+       points.color.a = 1.0f;
 	   //set point equal to generated points
 	   geometry_msgs::Point p;
 		p.x = choiceX;
         p.y = choiceY;
-        p.z = 1.0;
+        p.z = 0;
 		points.points.push_back(p);
 		count++;
 
+		
 		marker_pub.publish(points);
         ros::spinOnce();
         loop_rate.sleep();
@@ -76,37 +81,22 @@ int main(int argc, char **argv)
 }
 
 
-void generatePoints(vector<vector<double>> &points,double sizeInSquareMeters, double minXvalue, double minYvalue, double maxXvalue, double distancebetweenpoints){
+void generatePoints(vector<coordinatepoint> &points,double sizeonX, double sizeonY, double minXvalue, double minYvalue, double distancebetweenpoints){
 
-	int n = (int)sizeInSquareMeters/(int)distancebetweenpoints;
-	int m = 1;
-	vector<vector<double>> vec(n, vector<double> (m));
-	points = vec;
-	int count=0;
-	for (int i = 0; i <= ((n/(int)maxXvalue-(int)minXvalue) + 1); i++)
-	{ 
-		
-			for (int j = 0; j < (int)maxXvalue-(int)minXvalue; j++)
-			{
-				
-				points[count+i][0] = minXvalue + j;
-				points[count+i][1] = minYvalue + i;
-				cout << count << " ";
-				cout << points[count+i][0];
-				cout << " " << points[count+i][1] << endl;
-				count++;
+for (int i = 0; i <= sizeonY/distancebetweenpoints; i++)
+{
+	coordinatepoint temp;
+	for (int j = 0; j <= sizeonX/distancebetweenpoints; j++)
+	{
+		temp.x= minXvalue +(distancebetweenpoints*j);
+		temp.y = minYvalue + (distancebetweenpoints*i);
+		//cout << temp.x << " " << temp.y << endl;
+		points.push_back(temp);
+		//cout <<  minXvalue +(distancebetweenpoints*j) << " " << minYvalue + (distancebetweenpoints*i) << endl;
 			}
-			
-			
-	}
 	
-	//for (int k = 0; k < n; k++)
-//	{
-	//	cout << points[k][0] << " " << points[k][1]<<endl;
-		/* code */
-	////}
-	
-	
+}
+
 	
 }
 
